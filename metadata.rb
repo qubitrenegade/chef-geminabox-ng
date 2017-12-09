60dd2111f249602092e0b67163cb6943d3a4a204
qubitrenegade <qubitrenegade@gmail.com>
2017-12-09 13:19:08 -0700

    update metadata to pass foodcritic.

--- metadata.rb
+++ metadata.rb
@@ -7,17 +7,21 @@ long_description  'Installs/Configures geminabox-ng'
 version           '0.1.0'
 chef_version      '>= 12.1' if respond_to?(:chef_version)
 
+%w(centos rhel ubuntu arch).each do |platform|
+  supports platform
+end
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/geminabox-ng/issues'
+issues_url 'https://github.com/qubitrenegade/geminabox-ng/issues'
 
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/geminabox-ng'
+source_url 'https://github.com/qubitrenegade/geminabox-ng'
 
 depends 'ruby_rbenv'
 depends 'poise-service'
