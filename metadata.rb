33eee6a6a145807debf686b7cc8da7457d897658
qubitrenegade <qubitrenegade@gmail.com>
2017-12-11 00:10:15 +0000

    bumping version with fix

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'qubitrenegade@gmail.com'
 license           'MIT'
 description       'Installs/Configures geminabox-ng'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.1.0'
+version           '0.1.1'
 chef_version      '>= 12.1' if respond_to?(:chef_version)
 
 %w(centos rhel ubuntu arch).each do |platform|
@@ -21,7 +21,7 @@ issues_url 'https://github.com/qubitrenegade/geminabox-ng/issues'
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-source_url 'https://github.com/qubitrenegade/chef-geminabox-ng/tree/v0.1.0'
+source_url 'https://github.com/qubitrenegade/chef-geminabox-ng/tree/v0.1.1'
 
 depends 'ruby_rbenv'
 depends 'poise-service'
